47d2b4f0dc8ed185487cbf1e650bb0d09f2cf195
Akshay Paste <akshay.paste25@gmail.com>
2026-06-06 12:58:06 +0530

    Prime Number

---
+++ Main.java
@@ -0,0 +1,27 @@
+class Main {
+    public static void main(String[] args) {
+        int num = 12   ;
+        boolean isPrime = true;
+        
+        if (num <= 1){
+            System.out.println("Not Prime");
+        }
+        else{
+            for (int i = 2; i < num ; i++){
+                if (num % i == 0){
+                    System.out.println(num+" not prime number");
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if(isPrime){
+            System.out.println("Prime number");
+        }
+            
+    
+        
+        
+        
+    }
+}
