8c8c7e60592e3a7eb89510987d9f03d0de8d3945
Akshay2656 <akshay.paste25@gmail.com>
2026-06-21 11:37:54 +0530

    PerfectNumber

---
+++ PerfectNumber.java
@@ -0,0 +1,18 @@
+class PerfectNumber{
+	public static void main(String[] args){
+		int number = 6;
+		int sum = 0;
+		for (int i = 1; i <= number / 2;  i++){
+			if (number % i == 0){
+				sum = sum + i;
+			}
+		}
+		
+		if (sum == number){
+			System.out.println("Perfect Number");
+		}
+		else{
+			System.out.println("Not a perfect number");
+		}
+	}	
+}
