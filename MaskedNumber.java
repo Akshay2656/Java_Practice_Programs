ba707790b7ed233f25d4a87c3f4cd84e243b918c
Akshay2656 <akshay.paste25@gmail.com>
2026-06-21 16:34:08 +0530

    Masked Number

--- MaskedNumber.java
+++ MaskedNumber.java
@@ -4,6 +4,8 @@ class MaskedNumber{
 		
 		//System.out.println(cardNumber.replaceAll(cardNumber.substring(0,12),"************"));
 		
+		// repeat -> create stars equal to the number of digits to hide
+		// extract the last 4 digits and both part are concatenated to form the masked card number
 		String masked = "*".repeat(cardNumber.length() - 4) + cardNumber.substring(cardNumber.length() - 4);
 		
 		System.out.println(masked);
